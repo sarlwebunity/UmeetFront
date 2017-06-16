7302233f91e3f812fe954057ade703297051da28
Florian REBOUL <paul.bouyssounouse@gmail.com>
2017-06-16 23:14:52 +0200

    [config]

---
+++ typings/browser.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="browser\ambient\angular-resource\index.d.ts" />
+/// <reference path="browser\definitions\moment\index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="main\ambient\angular-resource\index.d.ts" />
+/// <reference path="main\ambient\angular-ui-bootstrap\index.d.ts" />
+/// <reference path="main\ambient\angular-ui-router\index.d.ts" />
+/// <reference path="main\ambient\angular\index.d.ts" />
+/// <reference path="main\ambient\jquery\index.d.ts" />
+/// <reference path="main\ambient\toastr\index.d.ts" />
+/// <reference path="main\definitions\moment\index.d.ts" />
